8f87681ea20383013cda26c7a127698e0cdc21b4
Sofia Krsmanovic <38912502+sofiakrsmanovic@users.noreply.github.com>
2018-06-25 02:17:39 -0500

    dalmatians for loop

---
+++ dalmatians-for-loop.js
@@ -0,0 +1,6 @@
+for (safeDalmatians = 2; safeDalmatians <101; safeDalmatians++)
+{
+	console.log("There are " + safeDalmatians + " dalmatians safe! Time to rescue another one!");
+}
+
+console.log("Hooray! All " + safeDalmatians + " dalmatians are safe! Roll the credits!");
